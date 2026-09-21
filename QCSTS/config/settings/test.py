2b229a5f6df208ca78ec11b9503ee00a2938f1eb
Ahmed abdullah <122040501+D-engahmed@users.noreply.github.com>
2026-09-21 04:28:37 +0300

    test(auth): use sufficiently long JWT signing key

--- QCSTS/config/settings/test.py
+++ QCSTS/config/settings/test.py
@@ -40,3 +40,5 @@ FRONTEND_PUBLIC_URL = "http://testserver"
 MFA_ENCRYPTION_KEY = "FNO0V6hoFD3uOI4ZhQbR_rV7bBbiEUdZaz0o-jRM7Lg="
 
 PAYMOB_HMAC_SECRET = "test-secret"
+
+SECRET_KEY = "ci-test-signing-key-0123456789abcdef0123456789abcdef"
